8f134267975a8c4f9b9e2564033ded2201e16f29
Slava <slavapv85@gmail.com>
2019-06-07 10:53:33 +0300

    description

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <meta name="description" content="Questions and answers"/>
 
 
     <!-- Fonts -->
